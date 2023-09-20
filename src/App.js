4fd048c0e9b52e11fc5a65ef7cb23a09c307da75
Aashu Tiwari <ashutripathi573@gmail.com>
2023-09-21 00:29:32 +0530

    Responsive Quiz app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './Pages/Home/Home';
+import Quiz from './Pages/Quiz/Quiz';
+import Results from './Pages/Results/Results';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const[name,setname]= useState("");
+  const[questions,setquestions]= useState();
+  const[score,setscore]= useState(0);
+
+  const fetchQuestions = async(category="",difficulty="")=>{
+    const {data} = await axios.get(
+      `https://opentdb.com/api.php?amount=20${
+        category && `&category=${category}`}${
+          difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    )
+    console.log(data.results)
+    setquestions(data.results)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <div className="app">
+          <Header/>
+          <Routes>
+            <Route path='/' element={<Home name={name} setname={setname} fetchQuestions={fetchQuestions} />} exact3>
+            </Route>
+            <Route path='/Quiz' element={<Quiz name={name} questions={questions} score={score} setscore={setscore} setquestions={setquestions} />} exact3>
+            </Route>
+            <Route path='/result' element={<Results score={score} />} exact3>
+            </Route>
+            
+          </Routes>
+        </div>
+        <Footer />
+    </BrowserRouter>
   );
 }
 
